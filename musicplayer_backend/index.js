3a38ec5d4e1dd3e5ba58526f19cbc78eff3345c6
Brijesh-Adeshara <brijeshsoni98245@gmail.com>
2024-08-16 14:05:48 +0530

    connected to mongodb atlas using mongoose

--- musicplayer_backend/index.js
+++ musicplayer_backend/index.js
@@ -1,6 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config();
 const port = 8000;
+
+mongoose
+  .connect(
+    "mongodb+srv://Brijesh:" +
+      encodeURIComponent(process.env.PASSWORD) +
+      "@cluster0.td0p9e7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((x) => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log("not connected");
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World");
