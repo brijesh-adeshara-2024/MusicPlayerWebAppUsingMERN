aa2eebfb051a8315029f6d0c5ab17f9b8668c9eb
Brijesh-Adeshara <brijeshsoni98245@gmail.com>
2024-08-16 18:12:37 +0530

    create get songs by artist name and get songs by songname api

--- musicplayer_backend/index.js
+++ musicplayer_backend/index.js
@@ -31,17 +31,17 @@ let opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.SECRET_KEY;
 passport.use(
-  new JwtStrategy(opts, function (jwt_payload, done) {
-    User.findOne({ id: jwt_payload.sub }, function (err, user) {
-      if (err) {
-        return done(err, false);
-      }
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    const user = await User.findOne({ id: jwt_payload.sub });
+    try {
       if (user) {
         return done(null, user);
       } else {
         return done(null, false);
       }
-    });
+    } catch (err) {
+      return done(err, false);
+    }
   })
 );
 
--- musicplayer_backend/routes/song.js
+++ musicplayer_backend/routes/song.js
@@ -2,16 +2,67 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const Song = require("../models/Song");
+const User = require("../models/User");
 
-router.post("/create", passport.authenticate("user"), async (req, res) => {
-  const { name, thumbnail, track } = req.body;
-  if (!name || !thumbnail || !track) {
-    return res.status(301).json({ error: "insufficient details provided" });
+router.post(
+  "/create",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      const { name, thumbnail, track } = req.body;
+      if (!name || !thumbnail || !track) {
+        return res.status(400).json({ error: "Insufficient details provided" });
+      }
+
+      const artist = req.user._id;
+      const songDetails = { name, thumbnail, track, artist };
+      const createdSong = await Song.create(songDetails);
+      return res.status(201).json(createdSong);
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
   }
-  const artist = req.user._id;
-  const songDetails = { name, thumbnail, track, artist };
-  const createdSong = await Song.create(songDetails);
-  return res.status(200).json(createdSong);
-});
+);
+
+router.get(
+  "/get/mysongs",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      const currentUser = req.user;
+      const songs = await Song.find({ artist: currentUser._id });
+      return res.json({ data: songs });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  }
+);
+
+// get song by artist
+
+router.get(
+  "/get/artist",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { artistId } = req.body;
+    const artist = await User.find({ _id: artistid });
+    if (!artist) {
+      return res.status(301).json({ error: "Artist does not exist" });
+    }
+    const songs = await Song.find({ artist: artistId });
+    return res.json({ data: songs });
+  }
+);
+
+// get song by song name
+router.get(
+  "get/songname",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { songName } = req.body;
+    const songs = await Song.find({ name: songName });
+    return res.json({ data: songs });
+  }
+);
 
 module.exports = router;
